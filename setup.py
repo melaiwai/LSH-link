22c21883d11fbd1e623171f7e918a5af87990155
melaiwai <31319955+melaiwai@users.noreply.github.com>
2018-05-02 22:06:59 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = "lsh",
+      version = "1.0",
+      author='Melanie Lai Wai and Weisong Zhu',
+      author_email='',
+      url='',
+      py_modules = ['LSH-link'],
+      packages=setuptools.find_packages(),
+      scripts = ['run_sta663.py'],
+      python_requires='>=3',
+      )
